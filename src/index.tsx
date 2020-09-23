beda0149b3e7a46801bc9d7703cf3a60ac82c162
garrygu <garry.guzy@gmail.com>
2020-09-23 10:21:58 +0800

    fix(types): update types for less

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react'
-import styles from './styles.module.css'
+import styles from './styles.module.less'
 
 interface Props {
   text: string
 }
 
 export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+  return <div className={styles.title}>Example Component: <span className="text">{text}</span></div>
 }
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -6,6 +6,14 @@ declare module '*.css' {
   const content: { [className: string]: string };
   export default content;
 }
+declare module '*.less' {
+  const content: { [className: string]: string };
+  export default content;
+}
+declare module '*.scss' {
+  const content: { [className: string]: string };
+  export default content;
+}
 
 interface SvgrComponent extends React.StatelessComponent<React.SVGAttributes<SVGElement>> {}
 
